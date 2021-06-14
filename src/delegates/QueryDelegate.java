a9cc454cf374a7f90c39a1823a567487c8c007a5
Jeffrey <jchen727@students.cs.ubc.ca>
2021-06-14 16:32:27 -0700

    delegate updated all query functions implemented

--- src/delegates/QueryDelegate.java
+++ src/delegates/QueryDelegate.java
@@ -2,6 +2,7 @@ package delegates;
 
 import model.Booking;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import model.*;
@@ -10,11 +11,11 @@ public interface QueryDelegate {
 
     public boolean insertVacationPlan(int planID, Date startDate, Date endDate, double price);
     public boolean deleteCustomer(String email);
-    public boolean updateReview(int reviewID);
+    public boolean updateReview(int reviewID, String description);
     public List<VacationPlan> selectVacationPrice(double price);
     public List<Integer> selectVacationDestination(String country);
-    public List<List<String>> selectCruise(int rating);
+    public List<ArrayList<String>> selectCruise(int rating);
     public double selectReviewAverage(int planID);
-    public List<List<Integer>> eventCount();
+    public List<ArrayList<Integer>> eventCount();
     public List<VacationPlan> vacationBookedByAll();
 }
